384f508e5cbc8a185b7b5bc0346290e01c915327
saajunaid <56006328+saajunaid@users.noreply.github.com>
2019-11-27 15:39:24 +0000

    Create General Learning.d

---
+++ General Learning.d
@@ -0,0 +1,6 @@
+# General Concepts
+
+* [Difference between SOAP & REST API](https://www.upwork.com/hiring/development/soap-vs-rest-comparing-two-apis/)
+* [Linux From Scratch](http://www.linuxfromscratch.org/lfs/)  
+* [Xprintidle](https://github.com/g0hl1n/xprintidle)
+    * xprintidle is a utility that queries the X server for the user's idle time and prints it to stdout (in milliseconds).
